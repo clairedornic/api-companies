e67ebe4edb42c0c4a312513e0eff94382d33f7a9
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-08-16 22:36:47 +0200

    feat (config database) : add connection informations

---
+++ config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const config = {
+    db: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      connectTimeout: 60000
+    },
+ };
+module.exports = config;
