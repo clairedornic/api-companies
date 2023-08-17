51a9d76248c7fe18d92e49996c9ca9a9fd9ee7d0
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-08-17 17:54:59 +0200

    feat: add routing to get companies (filter them by name and by page), get establishments, get company and establishment by id

--- app.js
+++ app.js
@@ -1,12 +1,124 @@
 const express = require('express')
-const db = require('./services/db');
+require('./services/db');
+const importDataApi = require('./services/importDataApi');
 const app = express()
-const port = 3000
+const port = 3000;
 
+const knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  }
+});
+
+/**
+ * Welcome URL
+ */
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Welcome to the company api created for Kabolt !')
 })
 
+/**
+ * Get all companies
+ * Handle pagination
+ * Filter by name
+ */
+app.get('/companies', async (req, res) => {
+  const currentPage = parseInt(req.query.page) || 1;
+  const itemsPerPage = 5;
+  const offset = (currentPage - 1) * itemsPerPage;
+  const nameFilter = req.query.name || '';
+
+  try {
+    const companies = await knex('companies')
+      .where('name', 'like', `%${nameFilter}%`)
+      .offset(offset)
+      .limit(itemsPerPage);
+
+    res.json(companies);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred when retrieving the companies.' });
+  }
+});
+
+/**
+ * Get company by ID
+ */
+app.get('/companies/:id', async (req, res) => {
+  const companyId = req.params.id;
+
+  try {
+    const company = await knex('companies')
+      .where('id', companyId)
+      .first();
+
+    if (!company) {
+      return res.status(404).json({ error: 'Company not found.' });
+    }
+
+    res.json(company);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred during the recovery of the company.' });
+  }
+});
+
+/**
+ * Get all establishments
+ */
+app.get('/establishments', async (req, res) => {
+  try {
+    const establishments = await knex('establishments').select('*');
+    res.json(establishments);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred when retrieving the companies.' });
+  }
+});
+
+/**
+ * Get establishment by ID
+ */
+app.get('/establishments/:id', async (req, res) => {
+  const establishmentId = req.params.id;
+
+  try {
+    const establishment = await knex('establishments')
+      .where('id', establishmentId)
+      .first();
+
+    if (!establishment) {
+      return res.status(404).json({ error: 'Establishment not found.' });
+    }
+
+    res.json(establishment);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred when retrieving the facility.' });
+  }
+});
+
+/**
+ * Get all company's establishments with company's ID
+ */
+app.get('/establishments/company/:id', async (req, res) => {
+  const companyId = req.params.id;
+
+  try {
+    const establishments = await knex('establishments')
+      .where('company_id', companyId)
+      .select('*');
+
+    if (establishments.length === 0) {
+      return res.status(404).json({ error: 'No establishments found for this company.' });
+    }
+
+    res.json(establishments);
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred when retrieving the establishments.' });
+  }
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Kabolt API listening on port ${port}`)
 })
