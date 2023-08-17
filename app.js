628e4e51d94a66b1e2e7c557a7406fd86a02ca4e
clairedornic <50169132+clairedornic@users.noreply.github.com>
2023-08-17 17:59:50 +0200

    feat (routing) : add error handling

--- app.js
+++ app.js
@@ -119,6 +119,21 @@ app.get('/establishments/company/:id', async (req, res) => {
   }
 });
 
+/**
+ * Error 404
+ */
+app.use((req, res, next) => {
+  res.status(404).json({ error: 'URL not valid.' });
+});
+
+/**
+ * Internal error
+ */
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'An internal error has occurred.' });
+});
+
 app.listen(port, () => {
   console.log(`Kabolt API listening on port ${port}`)
 })
